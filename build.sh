9307d392b61035de1b0f3c8c7bb1b772edb9afff
Mathieu Vedie <mathieu.vedie@gmail.com>
2024-02-03 01:33:17 +0100

    fix

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ CUR_DIR=$(pwd)
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 PHP_FILE=$SCRIPT_DIR"/OneFichierCom.php"
 INFO_FILE=$SCRIPT_DIR"/INFO"
-VERSION=$(cat "$PHP_FILE" | grep "@Version :" | awk '{print $3}')
+VERSION=$(cat "$INFO_FILE" | grep version | awk -F  ':' '{print $2}' | awk -F '"' '{print $2}')
 cd "$SCRIPT_DIR"
 tar -czvf "OneFichierCom($VERSION).host" INFO OneFichierCom.php 
 cd "$CUR_DIR"
