84ce5f1118fc575f81db5cd0a6ed50e1e955e470
Mathieu Vedie <mathieu.vedie@gmail.com>
2024-02-03 01:16:08 +0100

    Remove echo from build script

--- build.sh
+++ build.sh
@@ -3,8 +3,6 @@ CUR_DIR=$(pwd)
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 PHP_FILE=$SCRIPT_DIR"/OneFichierCom.php"
 INFO_FILE=$SCRIPT_DIR"/INFO"
-echo $PHP_FILE
-echo $INFO_FILE
 VERSION=$(cat "$PHP_FILE" | grep "@Version :" | awk '{print $3}')
 cd "$SCRIPT_DIR"
 tar -czvf "OneFichierCom($VERSION).host" INFO OneFichierCom.php 
